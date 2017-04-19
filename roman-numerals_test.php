07fa02b71d2e315552d1da2b956e22d09f360ad9
Mike Thomas <mike@Mikes-MacBook-Pro.local>
2017-04-19 14:55:39 +0100

    first commit

---
+++ roman-numerals_test.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "roman-numerals.php";
+
+class RomanTest extends PHPUnit\Framework\TestCase
+{
+    public function test1()
+    {
+        $this->assertSame('I', toRoman(1));
+    }
+
+    /* More tests... */
+}
